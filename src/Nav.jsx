ff05232c40df4e04de4993a21941a783c1125145
Rishi Raj Gautam <162585878+rishi-raj-gautam@users.noreply.github.com>
2024-09-03 11:03:07 +0530

    navbar updated

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -11,15 +11,14 @@ function Nav() {
                 <div className="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo01">
                 <div className="navbar-nav ">
                         
-                        <a className="nav-link active mx-2 px-3 my-1" aria-current="page" href="#about">About</a>
-                        <a className="nav-link active mx-2 px-3 my-1" aria-current="page" href="#schedule">Schedule</a>
+                        <a className="nav-link active mx-1 px-3 my-1" aria-current="page" href="#about">About</a>
+                        <a className="nav-link active mx-1 px-3 my-1" aria-current="page" href="#schedule">Schedule</a>
                         
                         
 
-                       <button className="button mx-2 px-3 my-1">
-                            <a href='https://forms.gle/9d3Bd8hEGX4so24p8' target='_blank'><span>Regsiter Now</span></a>
-                            <span></span>
-                        </button>
+                        <a href='https://forms.gle/9d3Bd8hEGX4so24p8' target='_blank'>
+                        <button className='btn mx-2 px-3 py-2 my-1'>Register Now</button>
+                    </a>
                     </div>
                 </div>
             </div>
