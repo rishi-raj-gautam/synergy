2c95b653f1092a4ceaeae9e0658621ded5e14ecf
Rishi Raj Gautam <162585878+rishi-raj-gautam@users.noreply.github.com>
2024-09-04 21:07:03 +0530

    speaker added

--- src/Speakers.jsx
+++ src/Speakers.jsx
@@ -1,50 +1,58 @@
 import Sach from './assets/Sach.jpg'
 import Abhi from './assets/Abhi.jpg'
 import Adv from './assets/Advisor.jpg'
+import Vishal from './assets/Vishal.jpg'
 import './speaker.css'
-function Speaker(){
+function Speaker() {
     return <>
-    <div className='container-fluid speaker' id="speakers">
-        <div><h2 className='sch my-5 text-center'>Speakers</h2></div>
-        <div id="carouselExampleCaptions" className="carousel slide">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src={Sach} className="d-block w-100" alt="..."/>
-      <div className="carousel-caption d-md-block">
-        <h5>Dr. Sachin Ahuja</h5>
-        <p>Executive Director, UIE</p>
-      </div>
-    </div>
-    <div className="carousel-item">
-      <img src={Adv} className="d-block w-100" alt="..."/>
-      <div className="carousel-caption d-md-block">
-        <h5>Dr. Sartajvir Singh Dhillon</h5>
-        <p>Associate Director, UIE</p>
-      </div>
-    </div>
-    <div className="carousel-item">
-      <img src={Abhi} className="d-block w-100" alt="..."/>
-      <div className="carousel-caption  d-md-block">
-        <h5>Abhishek Pandey</h5>
-        <p>Assistant Director CSE, UIE</p>
-      </div>
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
-</div>
+        <div className='container-fluid speaker' id="speakers">
+            <div><h2 className='sch my-5 text-center'>Speakers</h2></div>
+            <div id="carouselExampleCaptions" className="carousel slide">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={Sach} className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-md-block">
+                            <h5>Dr. Sachin Ahuja</h5>
+                            <p>Executive Director, UIE</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={Adv} className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-md-block">
+                            <h5>Dr. Sartajvir Singh Dhillon</h5>
+                            <p>Associate Director, UIE</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={Vishal} className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-md-block">
+                            <h5>Prof. Vishal Dutt</h5>
+                            <p>Technical Trainer CSE, UIE</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={Abhi} className="d-block w-100" alt="..." />
+                        <div className="carousel-caption  d-md-block">
+                            <h5>Abhishek Pandey</h5>
+                            <p>Assistant Director CSE, UIE</p>
+                        </div>
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
     </>
 }
 export default Speaker;
